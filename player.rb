caf5f82db1e945ac2288ff1ceab3bcb5fc249301
DercilioFontes <dercilioafontes@gmail.com>
2018-02-27 08:20:02 +0000

    Implemeted inicial Player class and tested.

---
+++ player.rb
@@ -0,0 +1,29 @@
+# The player must answer correctly or lose a life.
+
+class Player
+  attr_accessor :lives, :turn
+
+  def initialize
+    @lives = 3
+    @turn = true # check!
+  end
+
+  # Take a turn and change turn. If lost (!win), loses a life. 
+  def take_turn(win)
+    if !win
+      self.lives -= 1
+    end
+    self.turn = self.turn ? false : true
+  end
+
+end
+
+# p1 = Player.new
+
+# puts p1.lives
+# puts p1.turn
+# puts p1.take_turn(true)
+# puts p1.turn
+# puts p1.lives
+# puts p1.take_turn(false)
+# puts p1.lives
---
+++ two-o-game.rb
@@ -0,0 +1,71 @@
+# Example prompt
+# Player 1: What does 5 plus 3 equal?
+# > 9
+# Player 1: Seriously? No!
+# P1: 2/3 vs P2: 3/3
+# ----- NEW TURN -----
+# Player 2: What does 2 plus 6 equal?
+# > 8
+# Player 2: YES! You are correct.
+# P1: 2/3 vs P2: 3/3
+# ----- NEW TURN -----
+# ... some time later ...
+# Player 1 wins with a score of 1/3
+# ----- GAME OVER -----
+# Good bye!
+
+
+
+# 2 players take turns to answer simple math addition problems
+# The game doesn’t end until one of the players loses all their lives. At this point, the game should announce who won and what the other player’s score is.
+
+class MathGame
+
+  def initialize()
+    player1 = new Player('Player1')
+    player2 = new Player('Player2')
+  end
+
+  def play
+    # game loop (checke current_player)
+    # as long as there's still game, create a turn, run it, print results-so-far
+  end
+
+  def check_end_game
+  end
+
+  def print_game_status
+  end
+  
+end
+
+
+
+# new math question is generated for each turn by picking 2 numbers between 1 and 20
+# At the end of every turn, the game should output the new scores for both players, so players know where they stand.
+class Turn
+  
+  def initialize(player)
+    question = new Question
+    
+  end
+
+  def play
+    # Print the question (get from the Question and the answer) and the player that get form the game
+  end
+
+end
+
+
+class Question
+  def initialize
+    n1 = Math.random
+    n2 = 
+  end
+
+  def question
+  end
+
+  def test_answer(attempt)
+  end
+end
